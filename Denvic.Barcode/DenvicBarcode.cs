16ef75e65b211ab940f7efeb63f3240038032ec4
pimenov <dr.p.sergio@gmail.com>
2017-03-23 14:27:35 +0300

    dr.p.sergio@gmail.com

--- Denvic.Barcode/DenvicBarcode.cs
+++ Denvic.Barcode/DenvicBarcode.cs
@@ -16,106 +16,14 @@ using ZXing.QrCode.Internal;
 
 namespace Denvic.Barcode
 {
-    //[InterfaceTypeAttribute(1)]
-    //[ComConversionLossAttribute]
-    //[GuidAttribute("B196B28D-BAB4-101A-B69C-00AA00341D07")]
-    //public interface IPropertyPage1
-    //{
-    //    void Activate(IntPtr HwndPtr, RECT pRect, Int32 bModal);
-    //    void Apply();
-    //    void Deactivate();
-    //    void GetPageInfo(ref PROPPAGEINFO pPageInfo);
-    //    void Help([In, MarshalAs(UnmanagedType.LPWStr)] string pszHelpDir);
-    //    [PreserveSig]
-    //    int IsPageDirty();
-    //    void Move(ref RECT pRect);
-    //    void SetObjects(uint cObject, [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.IUnknown, SizeParamIndex = 0)] object[] pPunk);
-    //    void SetPageSite(IPropertyPageSite pPageSite);
-    //    void Show(uint mCmdShow);
-    //    [PreserveSig]
-    //    int ranslateAccelerator(ref Message pMsg);
-    //}
-
-    //public class ExamplePropertyPage : IPropertyPage
-    //{
-    //    PropertyGrid pGrid;
-
-    //    //[DllImport("user32.dll", SetLastError = true)]
-    //    //private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
-
-    //    public void Activate(IntPtr hWndParent, RECT[] pRect, int bModal)
-    //    {
-    //        pGrid = new PropertyGrid();
-    //        pGrid.CreateControl();
-    //        SetParent(pGrid.Handle, hWndParent);
-    //    }
-
-    //    public int Apply()
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public void Deactivate()
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public void GetPageInfo(PROPPAGEINFO[] pPageInfo)
-    //    {            
-    //        pPageInfo[0].cb = (UInt32)Marshal.SizeOf(typeof(PROPPAGEINFO));
-    //        pPageInfo[0].pszTitle = "TextBarcode";
-    //    }
-
-    //    public void Help(string pszHelpDir)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public int IsPageDirty()
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public void Move(RECT[] pRect)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public void SetObjects(uint cObjects, object[] ppunk)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public void SetPageSite(IPropertyPageSite pPageSite)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public void Show(uint nCmdShow)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public int TranslateAccelerator(MSG[] pMsg)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-    //}
-
-
     [ComVisible(true)]
     [ProgId("Denvic.Barcode")]
     [Guid("9FAB265B-867F-49D2-9CDC-75E2851B1060")]
     [ClassInterface(ClassInterfaceType.None)]
-    //[ComDefaultInterface(typeof(IDenvicBarcode))]
-    //[ComSourceInterfaces(typeof(UserControlEvents))]
-    //[System.ComponentModel.EditorBrowsable(EditorBrowsableState.Never)]
-    public partial class ActiveXBarcode : UserControl, IDenvicBarcode//, ISpecifyPropertyPages//, IObjectSafety
+    public partial class ActiveXBarcode : UserControl, IDenvicBarcode
     {
-        //private PictureBox background;
         private string textBarcode;
         private Bitmap BarcodeImage;
-        //private IContainer components;
         private BarcodeFormat _barcodeFormat = BarcodeFormat.QR_CODE;
 
         public ActiveXBarcode()
@@ -209,12 +117,16 @@ namespace Denvic.Barcode
             }
         }
 
+        /// <summary>
+        /// Инициализация объекта ШК
+        /// </summary>
+        /// <param name="text"></param>
         private void GenerateBarcode(string text)
         {
             var writer = new BarcodeWriter
             {
                 Format = _barcodeFormat,
-                Options = new  QrCodeEncodingOptions
+                Options = new QrCodeEncodingOptions
                 {
                     Width = this.Width * 2,
                     Height = this.Height * 2,
@@ -224,69 +136,62 @@ namespace Denvic.Barcode
             };
 
             BarcodeImage = writer.Write(text);
-
-            //Invalidate();
-
-            //ZXing.Common.BitMatrix byteIMGNew = writer.Encode(text);            
-            //sbyte[][] imgNew = byteIMGNew.Array;
-            //Bitmap bmp1 = new Bitmap(byteIMGNew.Width, byteIMGNew.Height);
-            //Graphics g1 = Graphics.FromImage(bmp1);
-            //g1.Clear(Color.White);
-            //for (int i = 0; i <= imgNew.Length - 1; i++)
-            //{
-            //    for (int j = 0; j <= imgNew[i].Length - 1; j++)
-            //    {
-            //        if (imgNew[j][i] == 0)
-            //        {
-            //            g1.FillRectangle(Brushes.Black, i, j, 1, 1);
-            //        }
-            //        else
-            //        {
-            //            g1.FillRectangle(Brushes.White, i, j, 1, 1);
-            //        }
-            //    }
-            //}
-
-            // BackgroundImage = bitmap;            
         }
 
+        #region registration
+
+        /// <summary>
+        /// Регистрация класса в реестре (обязательное условие для отображения в объектах ActiveX например в 1С)
+        /// </summary>
+        /// <param name="key"></param>
         [ComRegisterFunction()]
         public static void RegisterClass(string key)
-        {
+        {            
             var skey = new StringBuilder(key);
             skey.Replace(@"HKEY_CLASSES_ROOT\", "");
-            var regKey = Registry.ClassesRoot.OpenSubKey(skey.ToString(), true);
-            var ctrl = regKey.CreateSubKey("Control");
-            ctrl.Close();
-            var inprocServer32 = regKey.OpenSubKey("InprocServer32", true);
-            inprocServer32.SetValue("CodeBase", Assembly.GetExecutingAssembly().CodeBase);
-            regKey.CreateSubKey("Insertable");
-            inprocServer32.Close();
-            regKey.Close();
+            using (var regKey = Registry.ClassesRoot.OpenSubKey(skey.ToString(), true))
+            {
+                using (var ctrl = regKey.CreateSubKey("Control"))
+                {
+                    ctrl.Close();
+                }
+                using (var inprocServer32 = regKey.OpenSubKey("InprocServer32", true))
+                {
+                    inprocServer32.SetValue("CodeBase", Assembly.GetExecutingAssembly().CodeBase);
+                    regKey.CreateSubKey("Insertable");
+                    inprocServer32.Close();
+                }
+                regKey.Close();
+            }
         }
 
+        /// <summary>
+        /// Удаляем информацию из реестра после отмены регистрации
+        /// </summary>
+        /// <param name="key"></param>
         [ComUnregisterFunction()]        
         public static void UnregisterClass(string key)
         {
             StringBuilder skey = new StringBuilder(key);
             skey.Replace(@"HKEY_CLASSES_ROOT\", "");
-            RegistryKey regKey = Registry.ClassesRoot.OpenSubKey(skey.ToString(), true);
-            regKey.DeleteSubKey("Control", false);
-            regKey.DeleteSubKey("Insertable", false);
-            RegistryKey inprocServer32 = regKey.OpenSubKey("InprocServer32", true);
-            regKey.DeleteSubKey("CodeBase", false);
-            regKey.Close();
-        }
-
-        protected override void OnResize(EventArgs e)
-        {
-            base.OnResize(e);
-            if (!string.IsNullOrWhiteSpace(textBarcode))
+            using (var regKey = Registry.ClassesRoot.OpenSubKey(skey.ToString(), true))
             {
-               // GenerateBarcode(textBarcode);
+                regKey.DeleteSubKey("Control", false);
+                regKey.DeleteSubKey("Insertable", false);
+                using (var inprocServer32 = regKey.OpenSubKey("InprocServer32", true))
+                {
+                    regKey.DeleteSubKey("CodeBase", false);
+                }
+                regKey.Close();
             }
         }
+        #endregion
 
+        /// <summary>
+        /// Repaint after resizing
+        /// Отрисовка после изменения размера
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnSizeChanged(EventArgs e)
         {
             base.OnSizeChanged(e);
@@ -300,75 +205,35 @@ namespace Denvic.Barcode
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            base.OnPaint(e);
-
             using (var graphic = e.Graphics)
             {
-
                 graphic.PixelOffsetMode = PixelOffsetMode.HighQuality;
                 graphic.InterpolationMode = InterpolationMode.NearestNeighbor;
                 graphic.SmoothingMode = SmoothingMode.None;
                 graphic.CompositingQuality = CompositingQuality.Default;
                 graphic.CompositingMode = CompositingMode.SourceCopy;
 
-                if (!string.IsNullOrWhiteSpace(textBarcode))
+                if (string.IsNullOrWhiteSpace(textBarcode) == false)
                 {
                     GenerateBarcode(textBarcode);
                 }
 
                 if (BarcodeImage != null)
+                {
                     graphic.DrawImage(BarcodeImage, new Rectangle(0, 0, Width, Height), new Rectangle(0, 0, BarcodeImage.Width, BarcodeImage.Height), GraphicsUnit.Pixel);
+                }
             }
         }
 
         private void InitializeComponent()
         {
-            this.SuspendLayout();
+            SuspendLayout();
             // 
             // DenvicBarcode
             // 
-            this.Name = "ActiveXControl";
-            this.ResumeLayout(false);
+            Name = "ActiveXControl";
+            ResumeLayout(false);
 
-        }
-
-        //public void GetPages(CAUUID[] pPages)
-        //{
-
-        //pPages[0].cElems = 1;
-        //pPages[0].pElems = Marshal.AllocCoTaskMem(16);
-        //var g1 = typeof(ExamplePropertyPage).GUID;
-        //var b1 = g1.ToByteArray();
-        //for (int x = 0; x < 16; x++)
-        //{
-        //    Marshal.WriteByte(pPages[0].pElems, x, b1[x]);
-        //}
-        // }
-
-        //public void GetPages(ref Object pPages)
-        //{
-        //    MessageBox.Show("GetPages");
-        //}
-
-        //public enum ObjectSafetyOptions
-        //{
-        //    INTERFACESAFE_FOR_UNTRUSTED_CALLER = 0x00000001,
-        //    INTERFACESAFE_FOR_UNTRUSTED_DATA = 0x00000002,
-        //    INTERFACE_USES_DISPEX = 0x00000004,
-        //    INTERFACE_USES_SECURITY_MANAGER = 0x00000008
-        //};
-
-        //public int GetInterfaceSafetyOptions(ref Guid riid, out int pdwSupportedOptions, out int pdwEnabledOptions)
-        //{
-        //    ObjectSafetyOptions m_options = ObjectSafetyOptions.INTERFACESAFE_FOR_UNTRUSTED_CALLER | ObjectSafetyOptions.INTERFACESAFE_FOR_UNTRUSTED_DATA;
-        //    pdwSupportedOptions = (int)m_options;
-        //    pdwEnabledOptions = (int)m_options;
-        //    return 0;
-        //}
-
-        //public int SetInterfaceSafetyOptions(ref Guid riid, int dwOptionSetMask, int dwEnabledOptions)
-        //{
-        //    return 0;
-        //}
+        }        
     }
 }
--- Denvic.Barcode/TypeBarcodeEnum.cs
+++ Denvic.Barcode/TypeBarcodeEnum.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Globalization;
-using System.Linq;
-using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Denvic.Barcode
 {
@@ -27,62 +21,4 @@ namespace Denvic.Barcode
         [Description("DATA_MATRIX")]
         DATA_MATRIX = 5
     }
-
-
-
-    //class EnumTypeConverter : EnumConverter
-    //{
-    //    private Type _enumType;
-    //    /// <summary>Инициализирует экземпляр</summary>
-    //    /// <param name="type">тип Enum</param>
-    //    public EnumTypeConverter(Type type) : base(type)
-    //    {
-    //        _enumType = type;
-    //    }
-
-    //    public override bool CanConvertTo(ITypeDescriptorContext context,
-    //      Type destType)
-    //    {
-    //        return destType == typeof(string);
-    //    }
-
-    //    public override object ConvertTo(ITypeDescriptorContext context,
-    //      CultureInfo culture,
-    //      object value, Type destType)
-    //    {
-    //        FieldInfo fi = _enumType.GetField(Enum.GetName(_enumType, value));
-    //        DescriptionAttribute dna =
-    //          (DescriptionAttribute)Attribute.GetCustomAttribute(
-    //            fi, typeof(DescriptionAttribute));
-
-    //        if (dna != null)
-    //            return dna.Description;
-    //        else
-    //            return value.ToString();
-    //    }
-
-    //    public override bool CanConvertFrom(ITypeDescriptorContext context,
-    //      Type srcType)
-    //    {
-    //        return srcType == typeof(string);
-    //    }
-
-    //    public override object ConvertFrom(ITypeDescriptorContext context,
-    //      CultureInfo culture,
-    //      object value)
-    //    {
-    //        foreach (FieldInfo fi in _enumType.GetFields())
-    //        {
-    //            DescriptionAttribute dna =
-    //              (DescriptionAttribute)Attribute.GetCustomAttribute(
-    //                fi, typeof(DescriptionAttribute));
-
-    //            if ((dna != null) && ((string)value == dna.Description))
-    //                return Enum.Parse(_enumType, fi.Name);
-    //        }
-
-    //        return Enum.Parse(_enumType, (string)value);
-    //    }
-
-    //}
 }
